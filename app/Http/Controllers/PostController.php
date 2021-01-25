44473e66bbbf22e334fe744f63eaaf98c1484351
Nicola <nginvernizzi1@gmail.com>
2021-01-25 17:26:10 +0100

    update

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -96,7 +96,28 @@ class PostController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $data = $request->all();
+
+        $request->validate($this->ruleValidation());
+
+        $post = Post::find($id);
+
+        $data['slug'] = Str::slug($data['title'], '-');
+
+        if(!empty($data['path_img'])) {
+            if (!empty($post->path_img)) {
+                Storage::disk('public')->delete($post->path_img);
+            }
+            $data['path_img'] = Storage::disk('public')->put('images', $data['path_img']);
+        }
+
+        $updated = $post->update($data);
+
+        if($updated) {
+            return redirect()->route('posts.show', $post->slug);
+        } else {
+            return redirect()->route('home');
+        }
     }
 
     /**
